930155b52379b159eb7e9f20ebb54359f4c544c9
barakadanny <barakadan421@gmail.com>
2025-04-17 23:57:47 +0200

    feat(frontend): create Home component to structure main page layout

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,22 @@
+// import Navbar from "@/components/shared/navbar"
+import HeroSection from "@/components/sections/hero-section"
+import HowItWorksSection from "@/components/sections/how-it-works-section"
+import FeaturesSection from "@/components/sections/features-section"
+import CtaSection from "@/components/sections/cta-section"
+import WhyChooseUsSection from "@/components/sections/why-choose-us-section"
+// import Footer from "@/components/shared/footer"
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* <Navbar /> */}
+      <main className="flex-grow">
+        <HeroSection />
+        <HowItWorksSection />
+        <FeaturesSection />
+        <WhyChooseUsSection />
+        <CtaSection />
+      </main>
+      {/* <Footer /> */}
+    </div>
+  )
+}
